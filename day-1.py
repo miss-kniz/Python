a8bbea6ee3a23977497a404c22f073a254d7ef8e
Mehak-tbs <mehak.fatima@thinkbuiltsol.com>
2026-03-09 22:42:08 +0500

    Add day 1

---
+++ day-1.py
@@ -0,0 +1,26 @@
+# day-1 exercise 
+'''
+Create a calculator that can get 2 digit and perform operations on them
+'''
+
+
+'''
+What got covered
+1. data types
+2. input function
+3. type casting
+4. operators
+5. print function
+'''
+
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+print("\nThe value of a= ", a, "\nThe value of b= ", b)
+print("Addition", a+b)
+print("Subtraction", a-b)
+print("Division", a/b)
+print("Reminder", a%b)
+print("Multiplication", a*b)
+print("Power", a**b)
+print("Floor Division", a//b)
